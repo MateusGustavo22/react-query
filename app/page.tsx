f8d6b87aec68504ae9370794de6f5a82cb7a6d1c
MateusGustavo22 <githubmateus@gmail.com>
2023-12-15 19:14:38 -0300

    basic query

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
+  async function getRepos() {
+    const data = await fetch(
+      "https://api.github.com/users/MateusGustavo22/repos"
+    );
+    console.log(data);
+  }
+
+  const { data } = useQuery({
+    queryKey: ["repos"],
+    queryFn: getRepos,
+  });
+
   return <h1>Hello World</h1>;
 }
