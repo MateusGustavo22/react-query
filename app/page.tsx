8c9ee0f4bd9b8046e872dfeaf2add149d7129d8c
MateusGustavo22 <githubmateus@gmail.com>
2023-12-16 15:21:04 -0300

    data fetching and rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 export default function Home() {
   async function getRepos() {
     const data = await fetch(
       "https://api.github.com/users/MateusGustavo22/repos"
     );
-    console.log(data);
+
+    const repos = await data.json();
+
+    return repos;
   }
 
   const { data } = useQuery({
@@ -15,5 +19,22 @@ export default function Home() {
     queryFn: getRepos,
   });
 
-  return <h1>Hello World</h1>;
+  return (
+    <div className="w-max m-auto mt-8 flex flex-col gap-2">
+      <span className="text-white text-xl">Repositórios</span>
+      {data ? (
+        data?.map((repo) => (
+          <div className="bg-zinc-900 px-6 py-2" key={repo.id}>
+            <Link href={repo.html_url}>
+              <span className="text-gray-400 hover:text-white">
+                {repo.name}
+              </span>
+            </Link>
+          </div>
+        ))
+      ) : (
+        <span className="text-gray-400">Carregando...</span>
+      )}
+    </div>
+  );
 }
